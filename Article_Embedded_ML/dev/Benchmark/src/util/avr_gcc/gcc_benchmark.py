d3e031353c83cfc68a1c93b43471c185199dbce3
Vlad Baciu <baciuvladeusebiu@gmail.com>
2023-07-27 01:11:11 +0300

    ART2-DEV: add make clean command during build.

--- Article_Embedded_ML/dev/Benchmark/src/util/avr_gcc/gcc_benchmark.py
+++ Article_Embedded_ML/dev/Benchmark/src/util/avr_gcc/gcc_benchmark.py
@@ -11,25 +11,25 @@ class CompileAvrBenchmark:
         # Get model path to be compiled
         self.model_path, _ = os.path.splitext(build_info['runtime']['model_directory'])
 
-        #make_command = ["make", "clean"]
-        #try:
-        #    result = subprocess.run(
-        #        make_command,
-        #        shell=True,
-        #        stdout=subprocess.PIPE,
-        #        stderr=subprocess.PIPE,
-        #        cwd=self.avr_gcc_dir
-        #    )
-#
-        #    # Decode output from subprocess since it is a byte string
-        #    stdout_str = result.stdout.decode("utf-8")
-        #    stderr_str = result.stderr.decode("utf-8")
-#
-        #    print(stdout_str)
-        #    print(stderr_str)
-        #except subprocess.CalledProcessError as e:
-        #    # If the make command returns a non-zero exit code, it will raise CalledProcessError
-        #    print("Error occurred: {}".format(e))
+        make_command = ["make", "clean", "MODEL_PATH=" + r'{}'.format(self.model_path)]
+        try:
+            result = subprocess.run(
+                make_command,
+                shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                cwd=self.avr_gcc_dir
+            )
+
+            # Decode output from subprocess since it is a byte string
+            stdout_str = result.stdout.decode("utf-8")
+            stderr_str = result.stderr.decode("utf-8")
+
+            print(stdout_str)
+            print(stderr_str)
+        except subprocess.CalledProcessError as e:
+            # If the make command returns a non-zero exit code, it will raise CalledProcessError
+            print("Error occurred: {}".format(e))
 
     def compile(self):
         make_command = ["make", "MODEL_PATH=" + r'{}'.format(self.model_path)]
