16993f3177823de06d9b7dde4ba4fe7837333426
Vlad Baciu <baciuvladeusebiu@gmail.com>
2024-04-07 18:49:38 +0300

    Fix incorect cast for m2cgen.

--- src/port_m2cgen/template/infer_model.h
+++ src/port_m2cgen/template/infer_model.h
@@ -20,36 +20,20 @@ static inline void th_initModel(void) {}
 static inline void th_infer(void)
 {
     predict(&input[0], &output[0]);
-#if defined(SVC) || defined(LINEARSVC) || defined(NUSVC)
-    uint8_t thValue = 0U;
+    double thValue = 0U;
     uint8_t maxIdx = 0U;
 
     thValue = output[0];
     /* Determine model_output from output encoded value. */
     for(uint8_t i = 1; i < PY_OUTPUT_SIZE; i++)
     {
-        #if defined(LINEARSVC)
         if(output[i] > thValue)
-        #else
-        if(output[i] < thValue)
-        #endif
         {
             thValue = output[i];
             maxIdx = i;
         }
     }
     model_output[0] = maxIdx;
-#else
-    for(uint8_t i = 0; i < PY_OUTPUT_SIZE; i++)
-    {
-        if(output[i] == 1)
-        {
-            model_output[0] = i;
-            break;
-        }
-    }
-#endif /* defined(SVC) || defined(LINEARSVC) || defined(NUSVC) */
-
 }
 
 #ifdef __cplusplus
